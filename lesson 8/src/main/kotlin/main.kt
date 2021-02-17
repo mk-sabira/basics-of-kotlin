eabe95a536390d6d5e4f5524629574306b86feba
Sabira <mnsabira@gmail.com>
2021-02-17 21:00:08 +0400

    new folder lesson 8 was created

---
+++ lesson 8/src/main/kotlin/main.kt
@@ -0,0 +1,47 @@
+fun main(args: Array<String>) {
+////    array 1
+////    index 42 0 0 0 0
+////    index 0 1 2 3 4 5
+//
+//    val firstArray = Array(5){0}
+//
+//    firstArray[0] = 23
+//    firstArray[1] = -1
+//
+//    println(firstArray[0])
+//    println(firstArray[1])
+//    println(firstArray[5])
+
+//    Array 2
+
+    val firstArray:Array<Int> = arrayOf(1,2,3,4,5)
+
+
+//    println(firstArray[0])
+//    println(firstArray[1])
+//    println(firstArray[3])
+
+//    ArrayList 1
+
+//    val firstArrayList = ArrayList<String>()
+//
+//    firstArrayList.add("Kotlin")
+//    firstArrayList.add("Java")
+//    firstArrayList.add("Dart")
+//
+//    firstArrayList.add(1, "Programming")
+//    firstArrayList.removeAt(2)
+//
+//    println(firstArrayList[0])
+//    println(firstArrayList)
+//
+//    println(firstArrayList.size)
+
+    //    ArrayList 2
+
+    val SecondArrayList = arrayListOf<String>("Script", "C#")
+
+
+}
+
+
